e8a9a5821fd1b633f893e60b468430d06bf41a7f
DeveloperUmerFarooq <130483624+DeveloperUmerFarooq@users.noreply.github.com>
2024-07-16 19:36:13 +0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,13 +18,13 @@ export default function Navbar({title='Portfolio',color=false}) {
           <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/projects">Projects</Link>
+          <Link className="nav-link active" to="/projects">Projects</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/about">About</Link>
+          <Link className="nav-link active" to="/about">About</Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/contact">Contact</Link>
+          <Link className="nav-link active" to="/contact">Contact</Link>
         </li>
       </ul>
     </div>
